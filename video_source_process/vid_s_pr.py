0bc5512f6f5eaed554a85e3423dfdf2b90550fdf
ArtyomKozyrev8 <kozirev8@gmail.com>
2021-06-07 17:11:24 +0300

    remastered async ops in one class + added turn on/off sending frames to remote http server based on signals from remote server

--- video_source_process/vid_s_pr.py
+++ video_source_process/vid_s_pr.py
@@ -1,23 +1,39 @@
 import asyncio
 from datetime import datetime, timezone
 from io import BytesIO
+from enum import Enum
 import logging
 import time
 
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientWebSocketResponse, WSMsgType
 import cv2 as cv
 from PIL import Image, ImageFont, ImageDraw
 
+LOG_LEVEL = logging.INFO
+# you can choose any (not empty string) CAMERA_NAME, but should be unique if you have several processes
+# which get data from camera and send it to http server
+CAMERA_NAME = "cam_100500"
+URL = f"http://localhost:7474/video_remote_source/{CAMERA_NAME}"
+# RTMP should be 0 if you want to use default web camera
+# Otherwise you can provide rtmp link
+RTMP = 0
 
-URL = "http://localhost:7474/video_remote_source/cam_100500"
 
+# create log handler block
 app_log = logging.getLogger("app_log")
 app_log_handler = logging.StreamHandler()
 fmt = logging.Formatter("%(levelname)s | %(asctime)s | %(message)s")
 app_log_handler.setFormatter(fmt)
 app_log.addHandler(app_log_handler)
-app_log.setLevel(logging.INFO)
+app_log.setLevel(LOG_LEVEL)  # choose appropriate log level
 app_log.propagate = False
+
+
+class NotifyCamera(Enum):
+    """we expect two kind of messages from http server
+    which indicates whether we should continue sending frames to it"""
+    resume = b'1'
+    stop = b'0'
 
 
 class CameraHandle:
@@ -49,35 +65,79 @@ class CameraHandle:
         return new_image.getvalue()
 
 
-async def async_main(url: str):
-    """wrapper around all async ops in this process"""
-    app_log.info("Video Processing Process started")
-    while True:
-        cap = cv.VideoCapture(0)  # we open default web camera
-        async with ClientSession() as session:
-            ws = await session.ws_connect(url)
-            while True:
-                image_bytes = CameraHandle(cap).get_image_from_camera()
-                image_bytes = CameraHandle.add_timestamp(image_bytes)
-                if not image_bytes:
-                    break  # go to outer while
-                try:
+class AsyncOPs:
+    """Contains all async operations in this process"""
+    def __init__(self, url: str):
+        # we send frames to http server only if some client(browser) would
+        # like to receive such messages from http server
+        self.do_send = asyncio.Event()
+        self.url = url
+
+    async def send_data(self, cap: cv.VideoCapture, ws: ClientWebSocketResponse) -> None:
+        """Send video frame to remote http server through websocket"""
+        while True:
+            image_bytes = CameraHandle(cap).get_image_from_camera()
+            image_bytes = CameraHandle.add_timestamp(image_bytes)
+            if not image_bytes:
+                continue
+            try:
+                if self.do_send.is_set():
                     await ws.send_bytes(image_bytes)
-                except (ConnectionAbortedError, ConnectionResetError) as ex:
-                    app_log.error(f"Failed write to websocket: {ex}")
-                    # just start another iteration
-                except Exception as ex:
-                    app_log.error(f"Failed to write to websocket. Unexpected error: {ex}")
-                    break  # go to outer while
-        try:
-            cap.release()
-        except Exception as ex:
-            app_log.error(f"Failed to release camera capture: {ex}")
+                else:
+                    app_log.debug(f"Skip sending video frame to client")
+                    await asyncio.sleep(0)  # otherwise it wil block everything !
+            except (ConnectionAbortedError, ConnectionResetError) as ex:
+                app_log.error(f"Failed write to websocket: {ex}")
+                raise  # cope with it in outer scope
+
+    @staticmethod
+    async def monitor_active_coroutines() -> None:
+        """monitors whether we do garbage collection fine (do not create "zombie" coroutines)"""
+        while True:
+            tasks = len(asyncio.all_tasks())
+            app_log.debug(f"Number of active tasks: {tasks}")
+            await asyncio.sleep(15)
+
+    async def async_main(self) -> None:
+        app_log.info("Video Processing Process started")
+        asyncio.create_task(AsyncOPs.monitor_active_coroutines())
+        while True:
+            try:
+                self.do_send.set()
+                write_to_client_task = None
+                async with ClientSession() as session:
+                    app_log.info("Starting new session with http server")
+                    cap = cv.VideoCapture(RTMP)  # we open default web camera
+                    ws = await session.ws_connect(self.url)
+                    write_to_client_task = asyncio.create_task(self.send_data(cap=cap, ws=ws))
+                    async for msg in ws:
+                        if msg.type == WSMsgType.BINARY:
+                            # no client would like to get data from the camera
+                            if msg.data == NotifyCamera.stop.value:
+                                self.do_send.clear()
+                                app_log.debug(f"Stopped sending video to http server")
+                            # resume sending frames to http server
+                            else:
+                                self.do_send.set()
+                                app_log.debug("Resume sending video to http server")
+            except Exception as ex:
+                app_log.error(f"Unexpected error: {ex}")
+                time.sleep(5)
+            # we should release camera and cancel write task even if we had error
+            finally:
+                if write_to_client_task:
+                    try:
+                        cap.release()
+                        time.sleep(5)
+                    except Exception as ex:
+                        app_log.error(f"Failed to release camera capture: {ex}")
+                    try:
+                        write_to_client_task.cancel()
+                    except asyncio.CancelledError:
+                        app_log.error("Failed to cancel write_to_client_task")
+                    else:
+                        app_log.warning("write_to_client_task canceled")
+
 
 if __name__ == '__main__':
-    while True:
-        try:
-            asyncio.run(async_main(URL))
-        except Exception as ex:
-            app_log.error(f"Unexpected error: {ex}")
-            time.sleep(5)
+    asyncio.run(AsyncOPs(url=URL).async_main())
